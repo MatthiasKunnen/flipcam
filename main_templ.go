17aa2f024d540cb77eeeeb151a67067cfcd8ff24
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-05-14 04:59:50 +0200

    Prevent Firefox from keeping playlist path input value on refresh

--- main_templ.go
+++ main_templ.go
@@ -42,7 +42,7 @@ func html(playlistPath string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\"></div></aside><script type=\"module\" src=\"/static/main.mjs\"></script></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "\" autocomplete=\"off\"></div></aside><script type=\"module\" src=\"/static/main.mjs\"></script></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
