72c86307f2b16f3a41135734d42caf3bf3646fef
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-06-03 00:05:04 +0200

    Add autocomplete to --wireless-interface

--- internal/cmd/flipcam/shared.go
+++ internal/cmd/flipcam/shared.go
@@ -3,6 +3,10 @@ package flipcam
 import (
 	"github.com/spf13/cobra"
 	"log"
+	"net"
+	"os"
+	"path"
+	"strings"
 )
 
 func addHlsOutputDirFlag(cmd *cobra.Command, stringVar *string) {
@@ -28,16 +32,44 @@ func addHlsUrlPathPrefixFlag(cmd *cobra.Command, stringVar *string) {
 }
 
 func addInterfaceFlag(cmd *cobra.Command, stringVar *string) {
+	flagName := "wireless-interface"
 	cmd.Flags().StringVar(
 		stringVar,
-		"wireless-interface",
+		flagName,
 		"",
 		"Sets the name of the wireless interface to use.",
 	)
-	err := cmd.MarkFlagRequired("wireless-interface")
+	err := cmd.MarkFlagRequired(flagName)
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = cmd.RegisterFlagCompletionFunc(flagName, wifiInterfaceComplete)
+	if err != nil {
+		log.Fatalf("failed to register wireless interface completion: %v", err)
+	}
+}
+
+func wifiInterfaceComplete(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	matches := make([]string, 0)
+
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Printf("Error getting network interfaces: %v\n", err)
+		return nil, cobra.ShellCompDirectiveError
+	}
+
+	for _, iface := range interfaces {
+		_, err := os.Stat(path.Join("/sys/class/net", iface.Name, "wireless"))
+		if err != nil {
+			continue
+		}
+
+		if len(toComplete) == 0 || !strings.HasPrefix(iface.Name, toComplete) {
+			matches = append(matches, iface.Name)
+		}
+	}
+
+	return matches, cobra.ShellCompDirectiveDefault
 }
 
 func addIpv4Flag(cmd *cobra.Command, v *ipv4Flag) {
