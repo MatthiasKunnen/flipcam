03a40db287abf0c4c9ccba8ac0680253ec958b5e
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-06-05 17:54:24 +0200

    Remove debug println of sudo command

--- pkg/flipcamlib/util.go
+++ pkg/flipcamlib/util.go
@@ -2,9 +2,7 @@ package flipcamlib
 
 import (
 	"context"
-	"fmt"
 	"os/exec"
-	"strings"
 )
 
 func defaultString(value string, defaultVal string) string {
@@ -19,6 +17,5 @@ func sudoCommand(ctx context.Context, cmd []string) *exec.Cmd {
 	args := make([]string, len(cmd)+1)
 	args[0] = "--non-interactive"
 	copy(args[1:], cmd)
-	fmt.Println(strings.Join(args, " "))
 	return exec.CommandContext(ctx, "sudo", args...)
 }
