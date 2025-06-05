MatthiasKunnen/flipcam

commit 00f06085ec07fa783f44a1cc4b0fcb671d906c23

Default --hls-output-dir to /srv/flipcam/hls
In addition, configure the owner/group/permissions of the output dir
in the install script.

internal/cmd/flipcam/install.sh
@@ -81,3 +81,15 @@ fi
 echo "Setting ownership and permissions for /var/lib/caddy..."
 chown caddy:caddy /var/lib/caddy
 chmod 700 /var/lib/caddy
+
+if [ ! -d "{{.HlsOutputDir}}" ]; then
+  echo "Creating HLS output dir: {{.HlsOutputDir}}"
+  mkdir -p "{{.HlsOutputDir}}"
+fi
+
+chmod 2770 "{{.HlsOutputDir}}"
+chown root:flipcam "{{.HlsOutputDir}}"
+setfacl -m g:flipcam:rx "{{.HlsOutputDir}}"
+setfacl -m d:g:flipcamB:rx "{{.HlsOutputDir}}"
+
+# @todo check if caddy can read the HLS output dir

internal/cmd/flipcam/genconf.go
@@ -169,6 +169,7 @@ var genConfCmd = &cobra.Command{
 			"CaddyServiceName":   flipcam.ServiceNameCaddy(),
 			"DnsmasqConfPath":    flipcamlib.DnsmasqConfPath,
 			"DnsmasqServiceName": flipcam.ServiceNameDnsmasq(),
+			"HlsOutputDir":       hlsOutputDir,
 			"HostapdConfPath":    flipcamlib.HostapdConfPath,
 			"HostapdServiceName": flipcam.ServiceNameHostapd(),
 			"PolkitFilename":     polkitFilename,

internal/cmd/flipcam/shared.go
@@ -13,13 +13,9 @@ func addHlsOutputDirFlag(cmd *cobra.Command, stringVar *string) {
 	cmd.Flags().StringVar(
 		stringVar,
 		"hls-output-dir",
-		"",
+		"/srv/flipcam/hls",
 		"Sets the directory where the HLS segments and playlists are stored.",
 	)
-	err := cmd.MarkFlagRequired("hls-output-dir")
-	if err != nil {
-		log.Fatal(err)
-	}
 }
 
 func addHlsUrlPathPrefixFlag(cmd *cobra.Command, stringVar *string) {

commit 7054f8ee2b4d6296b3094a3f1e2eae459891f276

Fix incorrect setfacl command
Caused by 00f06085ec07fa783f44a1cc4b0fcb671d906c23.

internal/cmd/flipcam/install.sh
@@ -90,6 +90,6 @@ fi
 chmod 2770 "{{.HlsOutputDir}}"
 chown root:flipcam "{{.HlsOutputDir}}"
 setfacl -m g:flipcam:rx "{{.HlsOutputDir}}"
-setfacl -m d:g:flipcamB:rx "{{.HlsOutputDir}}"
+setfacl -m d:g:flipcam:rx "{{.HlsOutputDir}}"
 
 # @todo check if caddy can read the HLS output dir